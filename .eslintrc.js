2608f21d2648e0b51bc7140e35c3af1a369f8588
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-11-30 11:51:08 +0900

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ["airbnb", "plugin:prettier/recommended"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/require-default-props": "off"
+  }
+};
